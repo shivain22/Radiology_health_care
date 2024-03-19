964e003029c7c7c60de570a0d2997bca8139d3b9
Divineleo20 <divineleo20@gmail.com>
2024-03-19 15:33:00 +0530

    Added the initialization checkpoint for the project again

---
+++ lib/db/migrations/0000_handy_norrin_radd.sql
@@ -0,0 +1,16 @@
+CREATE TABLE `session` (
+	`id` varchar(255) NOT NULL,
+	`user_id` varchar(255) NOT NULL,
+	`expires_at` datetime NOT NULL,
+	CONSTRAINT `session_id` PRIMARY KEY(`id`)
+);
+--> statement-breakpoint
+CREATE TABLE `user` (
+	`id` varchar(255) NOT NULL,
+	`email` varchar(255) NOT NULL,
+	`hashed_password` varchar(255) NOT NULL,
+	`name` varchar(255),
+	CONSTRAINT `user_id` PRIMARY KEY(`id`)
+);
+--> statement-breakpoint
+ALTER TABLE `session` ADD CONSTRAINT `session_user_id_user_id_fk` FOREIGN KEY (`user_id`) REFERENCES `user`(`id`) ON DELETE no action ON UPDATE no action;
