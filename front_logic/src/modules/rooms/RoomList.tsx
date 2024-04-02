c799954e7997fc88ac3874a5e775c6a2c5e537ab
akankshrv <akankshvijay2109@gmail.com>
2024-04-02 15:24:46 +0530

    Rooms ui done

--- front_logic/src/modules/rooms/RoomList.tsx
+++ front_logic/src/modules/rooms/RoomList.tsx
@@ -13,8 +13,10 @@ import { RoomData } from "@/schema/rooms";
 
 import { deleteRoomAction } from "@/server_actions/actions/rooms";
 import { Roomform } from "./RoomForm";
+import { DataTable } from "./data-table";
+import { columns } from "./columns";
 
-type TOpenModal = (room?: RoomData) => void;
+export type TOpenModal = (room?: RoomData) => void;
 export default function RoomList({ rooms }: { rooms: RoomData[] }) {
   const [open, setOpen] = useState(false);
   const [activeRoom, setActiveRoom] = useState<RoomData | null>(null);
@@ -35,8 +37,11 @@ export default function RoomList({ rooms }: { rooms: RoomData[] }) {
         title={activeRoom ? "Edit Room" : "Add Room"}
       >
         <div>
-          <Roomform room={activeRoom} closeModal={closeModal} openModal={openModal} />
-          
+          <Roomform
+            room={activeRoom}
+            closeModal={closeModal}
+            openModal={openModal}
+          />
         </div>
       </Modal>
       <div className="absolute right-0 top-0 ">
@@ -44,66 +49,7 @@ export default function RoomList({ rooms }: { rooms: RoomData[] }) {
           +
         </Button>
       </div>
-      {rooms.length === 0 ? (
-        <EmptyState openModal={openModal} />
-      ) : (
-        <ul>
-          {rooms.map((room) => (
-            <Room room={room} key={room.id} openModal={openModal} />
-          ))}
-        </ul>
-      )}
+      <DataTable columns={columns} data={rooms} openModal={() => openModal()} />
     </div>
   );
 }
-
-//Displaying Individual Room Component
-const Room = ({
-  room,
-  openModal,
-}: {
-  room: RoomData;
-  openModal: TOpenModal;
-}) => {
-  const pathname = usePathname();
-  const basePath = pathname.includes("rooms")
-    ? pathname
-    : pathname + "/rooms/";
-
-  return (
-    <li className={cn("flex justify-between my-2")}>
-      <div className="w-full">
-        <div>{room.roomNo}</div>
-      </div>
-      <div className="flex gap-2 mr-5">
-        <Button variant={"link"} asChild>
-          <Link href={basePath + "/" + room.id}>Edit</Link>
-        </Button>
-        <Button
-            onClick={() => deleteRoomAction(room.id)}
-          variant={"destructive"}
-        >
-          Delete
-        </Button>
-      </div>
-    </li>
-  );
-};
-
-const EmptyState = ({ openModal }: { openModal: TOpenModal }) => {
-  return (
-    <div className="text-center">
-      <h3 className="mt-2 text-sm font-semibold text-secondary-foreground">
-        No Rooms
-      </h3>
-      <p className="mt-1 text-sm text-muted-foreground">
-        Get started by creating a new room.
-      </p>
-      <div className="mt-6">
-        <Button onClick={() => openModal()}>
-          <PlusIcon className="h-4" /> New Room{" "}
-        </Button>
-      </div>
-    </div>
-  );
-};
